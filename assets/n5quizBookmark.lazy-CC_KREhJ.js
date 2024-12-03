0c611140d9096b7cf884ddb4f4064cb43a73d192
fahrimz <mzfahri620@gmail.com>
2024-12-03 14:45:16 +0700

    Updates

---
+++ assets/n5quizBookmark.lazy-CC_KREhJ.js
@@ -0,0 +1,1 @@
+import{c as o,j as e}from"./index-ByMcUcDr.js";import{Q as t}from"./QuizPage-DfVrXH_K.js";import"./clsx-B9dzDlr9.js";const u=o("/n5quizBookmark")({component:r});function r(){return e.jsx(t,{quizType:"n5Bookmark",preferredTotalQuestions:10})}export{u as Route};
