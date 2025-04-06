d9f97fb7e25264173c3ce5854a38b5c31c9c9638
fahrimz <mzfahri620@gmail.com>
2025-04-06 15:05:09 +0700

    feat: add number shortcut for choosing answer

--- src/components/OptionButton.tsx
+++ src/components/OptionButton.tsx
@@ -1,8 +1,12 @@
 interface Props {
+  index: number;
   option: string;
   onClick: () => void;
 }
 
-export const OptionButton = ({ option, onClick }: Props) => {
-  return <button onClick={onClick} className="text-lg">{option}</button>;
+export const OptionButton = ({ index, option, onClick }: Props) => {
+  return <button onClick={onClick} className="flex flex-col gap-2">
+    <span className="text-lg">{option}</span>
+    <span className="text-[10px] text-gray-400">{index + 1}</span>
+  </button>;
 };
--- src/components/QuizView.tsx
+++ src/components/QuizView.tsx
@@ -1,6 +1,7 @@
 import { TQuizType, TWord } from "@data";
 import { OptionButton } from "./OptionButton";
 import { BookmarkView } from "./BookmarkView";
+import { useEffect } from "react";
 
 export const QuizView = ({
   quizType,
@@ -17,13 +18,30 @@ export const QuizView = ({
   answers: string[];
   checkAnswer: (option: string) => void;
 }) => {
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      answers.forEach((option, i) => {
+        if ((i + 1).toString() === e.key) {
+          checkAnswer(option);
+        }
+      })
+    }
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    }
+  })
+
   return (
     <div className="flex flex-col gap-8 justify-center">
       <h1>{currentQuestion.english}</h1>
       <div className="flex gap-4 flex-col sm:flex-row sm:justify-center">
-        {answers.map((option) => (
+        {answers.map((option, i) => (
           <OptionButton
             key={option}
+            index={i}
             option={option}
             onClick={() => {
               checkAnswer(option);
