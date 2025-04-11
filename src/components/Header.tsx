f71caa7c01cdaefd136f96368924b660dec1843e
fahrimz <mzfahri620@gmail.com>
2025-04-11 18:25:14 +0700

    fix: header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,9 +7,9 @@ interface HeaderProps {
 export const Header = ({ title }: HeaderProps) => {
     const { history } = useRouter();
     return (
-        <div className="bg-gray-200 p-4 mb-4 flex flex-row justify-between items-center w-full">
+        <div className="bg-white p-4 mb-4 flex flex-row justify-between items-center w-full">
             <IcArrowBack className="size-5 cursor-pointer" onClick={() => {history.go(-1)}} />
-            <span className="text-lg font-semibold">{title}</span>
+            <span className="text-lg font-semibold text-black">{title}</span>
         </div>
     )
 }
